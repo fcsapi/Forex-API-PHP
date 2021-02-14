5fe36d04d00cb873854b6b85b1262c3999999d7a
FCSAPI <59412356+fcsapi@users.noreply.github.com>
2021-02-14 19:58:44 +0500

    Add files via upload

---
+++ api/helper.php
@@ -0,0 +1,17 @@
+<?php
+
+
+function fcs_curl($url){
+	// create curl resource
+    $ch = curl_init();
+    // set url
+    curl_setopt($ch, CURLOPT_URL, $url);
+    //return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // $output contains the output string
+    $output = curl_exec($ch);
+    // close curl resource to free up system resources
+    curl_close($ch); 
+    return $output;
+}
+
