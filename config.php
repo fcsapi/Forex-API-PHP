b020490a108ea82c4c104819451b9219bfa0bc37
FCSAPI <59412356+fcsapi@users.noreply.github.com>
2021-02-14 19:57:36 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+
+/*======================================
+	You can use API with API KEY or IP 
+======================================*/
+
+// get your key here 	: https://fcsapi.com/dashboard
+// set your IP here 	: https://fcsapi.com/dashboard/profile
+
+$api_key = "API_KEY"; // signup to get your api_key 
+
+
+define("FCS_KEY",	$api_key); 
