d7ef40c32f5c6bb393e3b9b6b21d6ecb20c83f0f
FCSAPI <59412356+fcsapi@users.noreply.github.com>
2021-02-18 19:56:17 +0500

    Update helper.php

--- api/helper.php
+++ api/helper.php
@@ -1,13 +1,21 @@
 <?php
 
 
-function fcs_curl($url){
+function fcs_curl($url, $post){
 	// create curl resource
     $ch = curl_init();
     // set url
     curl_setopt($ch, CURLOPT_URL, $url);
     //return the transfer as a string
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // post parameters 
+    if(!empty($post)){
+        curl_setopt($ch, CURLOPT_POST, 1);
+        $post = is_array($post) ? http_build_query($post) : $post;
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    }
+
     // $output contains the output string
     $output = curl_exec($ch);
     // close curl resource to free up system resources
